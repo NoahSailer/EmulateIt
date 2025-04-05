18c7989e0ab6d70a36358d9149354f72ebafa400
NoahSailer <57497859+NoahSailer@users.noreply.github.com>
2025-04-05 09:37:25 -0700

    Update make_training_data.py

--- EmulateIt/make_training_data.py
+++ EmulateIt/make_training_data.py
@@ -12,18 +12,20 @@ def generate_training_data(num_samples, input_bounds, function_to_evaluate):
     outputs = np.array([function_to_evaluate(x) for x in inputs])
     return inputs, outputs
 
-def make_training_data(num_samples, input_fid, input_bounds, function_to_evaluate, train_dir='./'):
+def make_training_data(num_samples, input_fid, input_bounds, function_to_evaluate, train_dir='./', norm_type=None):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
     tdir = training_directory(train_dir)
+    output_fid = function_to_evaluate(input_fid)
+    if norm_type == 'division': new_function = lambda x: function_to_evaluate(x)/output_fid
+    else:                       new_function = lambda x: function_to_evaluate(x)
     if rank == 0: 
         print(f"Running {size} processes",flush=True)
-        output_fid = function_to_evaluate(input_fid)
         np.save(tdir.fid_in,input_fid)
         np.save(tdir.fid_out,output_fid)
     samples_per_proc = num_samples // size
-    local_inputs, local_outputs = generate_training_data(samples_per_proc, input_bounds, function_to_evaluate)
+    local_inputs, local_outputs = generate_training_data(samples_per_proc, input_bounds, new_function)
     gathered_inputs = comm.gather(local_inputs, root=0)
     gathered_outputs = comm.gather(local_outputs, root=0)
     if rank == 0:
