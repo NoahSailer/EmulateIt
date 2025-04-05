0d037860d7c5ff0aa6971266c0929af08260ad02
NoahSailer <57497859+NoahSailer@users.noreply.github.com>
2025-04-05 09:53:10 -0700

    Update matter_pk_emu.py

--- example/matter_pk_emu.py
+++ example/matter_pk_emu.py
@@ -4,7 +4,7 @@ import numpy as np
 
 cosmo_params = [3.04,0.96]
 pk_true = get_matter_pk(cosmo_params)
-mpk_emu = neural_network_emulator('mpk_weights.json')
+mpk_emu = neural_network_emulator(train_dir='./',norm_type='division')
 pk_emu  = mpk_emu.evaluate(cosmo_params)
 
 print("Ratio of emulated to true power spectrum:",pk_emu/pk_true)
