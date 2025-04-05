69f0aac6dbdd3a1e848321bc0fba6a4208edce13
NoahSailer <57497859+NoahSailer@users.noreply.github.com>
2025-04-05 09:41:09 -0700

    Update evaluate_neural_network.py

--- EmulateIt/evaluate_neural_network.py
+++ EmulateIt/evaluate_neural_network.py
@@ -1,3 +1,4 @@
+from EmulateIt.training_directory import training_directory 
 import numpy as np
 import json
 
@@ -12,22 +13,23 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 class neural_network_emulator:
-    def __init__(self,training_directory,norm_type=None):
+    def __init__(self,train_dir,norm_type=None):
         """
         training_directory = str ending with /
         norm_type = None, 'division', ...
         """
-        self.weights = load_model_weights(f"{training_directory}trained_weights.json")
-        self.fid_input = np.load(f"{training_directory}fid_input.npy")
-        self.fid_output = np.load(f"{training_directory}fid_output.npy")
-        self.norm_type = norm_type
+        tdir            = training_directory(train_dir)
+        self.weights    = load_model_weights(tdir.weights)
+        self.fid_input  = np.load(tdir.fid_in)
+        self.fid_output = np.load(tdir.fid_out)
+        self.norm_type  = norm_type
     def evaluate(self,inputs):
         """Performs a forward pass of the neural network."""
         layer_output = inputs
-        if self.norm_type == 'division': layer_output = inputs/self.fid_input
+        #if self.norm_type == 'division': layer_output = inputs/self.fid_input
         for i, (coef, intercept) in enumerate(zip(self.weights["coefs"], self.weights["intercepts"])):
             layer_output = np.dot(layer_output, np.array(coef)) + np.array(intercept)
-            if i < len(self.weights["coefs"]) - 1:  # Apply ReLU only to hidden layers
+            if i < len(self.weights["coefs"]) - 1:  
                 layer_output = np.tanh(layer_output)
         if self.norm_type == 'division': return layer_output*self.fid_output
         return layer_output
